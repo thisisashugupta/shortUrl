7be62128c934b00559d3577de47a990703009474
Ashu Gupta <ashugpt1812@gmail.com>
2023-07-22 23:30:19 +0530

    change in redis connection syntax

--- redis-client.js
+++ redis-client.js
@@ -1,12 +1,10 @@
 const Redis = require("ioredis");
-const redis = new Redis({
-  // Use the environment variables to configure the Redis client
-  url: process.env.REDIS_URL, // Redis server URL
-  //   host: process.env.REDISHOST,      // Redis server host
-  //   port: process.env.REDISPORT,      // Redis server port
-  //   password: process.env.REDISPASSWORD,  // Redis server password (if required)
-  //   username: process.env.REDISUSER,      // Redis server username (if required)
-  // Add any additional configuration options as needed
-});
+// new Redis("redis://username:authpassword@127.0.0.1:6380/4");
+const redis = new Redis(process.env.REDIS_URL);
+// Use the environment variables to configure the Redis client
+//   host: process.env.REDISHOST,
+//   port: process.env.REDISPORT,
+//   password: process.env.REDISPASSWORD,
+//   username: process.env.REDISUSER,
 
 module.exports = redis;
